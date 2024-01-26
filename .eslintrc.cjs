8c612360d24a21f6b32eb385a3d2318a4cccc892
Minh Hoang <mr.minhoan@gmail.com>
2024-01-26 17:17:49 +0700

    Config Theme MUI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,12 +16,22 @@ module.exports = {
     'react-refresh'
   ],
   rules: {
+    // React
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    // MUI
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
+
+    // Common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -39,6 +49,8 @@ module.exports = {
     'keyword-spacing': 1,
     'comma-dangle': 1,
     'comma-spacing': 1,
-    'arrow-spacing': 1
+    'arrow-spacing': 1,
+
+
   }
 }
