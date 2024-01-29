889c227182c01572114303f57d8022af81c4dbe5
Minh Hoang <mr.minhoan@gmail.com>
2024-01-29 17:17:30 +0700

    Refactor Project Structure

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,28 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import { cyan, deepOrange, orange, red, teal } from '@mui/material/colors';
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6'
-    },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
+const theme = extendTheme({
+  trelloCustom: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
   },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  }
+  // ...other properties
 });
 
 export default theme;
+
